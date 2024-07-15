b603a253be6cd73e1d8e6f7ef7ba354794884ac4
User <Neirox>
2024-07-15 03:06:15 +0300

    update

---
+++ my-app/components/shared/Select.vue
@@ -0,0 +1,36 @@
+<template>
+  <Select>
+    <SelectTrigger>
+      <SelectValue :placeholder="placeholder" />
+    </SelectTrigger>
+    <SelectContent>
+      <SelectGroup>
+        <SelectLabel>{{label}}</SelectLabel>
+        <SelectItem v-for="opt in options" :value="opt.value">
+          <Icon v-if="opt.icon" size="20" class="align-middle" :name="opt.icon" color="white"/>
+          {{ opt.alias }}
+        </SelectItem>
+      </SelectGroup>
+    </SelectContent>
+  </Select>
+</template>
+
+<script setup> 
+
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
+} from '@/components/ui/select'
+
+  defineProps({
+    options: Array,
+    label: String,
+    placeholder: String,
+  })
+
+</script>
--- my-app/components/widgets/CreateContent.vue
+++ my-app/components/widgets/CreateContent.vue
@@ -1,47 +1,64 @@
 <script setup lang="ts">
-import {
-  Dialog,
-  DialogContent,
-  DialogDescription,
-  DialogFooter,
-  DialogHeader,
-  DialogTitle,
-  DialogTrigger,
-} from '@/components/ui/dialog'
-
-import {
-  Select,
-  SelectContent,
-  SelectGroup,
-  SelectItem,
-  SelectLabel,
-  SelectTrigger,
-  SelectValue,
-} from '@/components/ui/select'
+// import {
+//   Dialog,
+//   DialogContent,
+//   DialogDescription,
+//   DialogFooter,
+//   DialogHeader,
+//   DialogTitle,
+//   DialogTrigger,
+// } from '@/components/ui/dialog'
 
 const TAGS_OBJECT = [
   {
     alias: 'JS',
+    value: 'vscode-icons:file-type-js-official',
     icon: 'vscode-icons:file-type-js-official',
   },
   {
     alias: 'TS',
+    value: 'vscode-icons:file-type-typescript-official',
     icon: 'vscode-icons:file-type-typescript-official',
   },
   {
     alias: 'Vue',
+    value: 'vscode-icons:file-type-vue',
     icon: 'vscode-icons:file-type-vue',
   },
   {
     alias: 'Окружение',
+    value: 'vscode-icons:file-type-docker',
     icon: 'vscode-icons:file-type-docker',
   },
   {
     alias: 'SOFT',
+    value: 'vscode-icons:file-type-jenkins',
     icon: 'vscode-icons:file-type-jenkins',
   },
 ]
 
+const TAGS = [
+  {
+    alias: 'Для собеседования',
+    value: 'Для собеседования',
+  },
+  {
+    alias: 'Развитие',
+    value: 'Развитие',
+  },
+  {
+    alias: 'Важно',
+    value: 'Важно',
+  },
+  {
+    alias: 'Окружение',
+    value: 'Окружение',
+  },
+  {
+    alias: 'SOFT',
+    value: 'SOFT',
+  },
+]
 
 </script>
 
@@ -71,50 +88,13 @@ const TAGS_OBJECT = [
           <div class="flex justify-between">
             <div class="w-full mr-2">
               <Label>Тэги</Label>
-              <Select>
-                <SelectTrigger>
-                  <SelectValue placeholder="Select a fruit" />
-                </SelectTrigger>
-                <SelectContent>
-                  <SelectGroup>
-                    <SelectLabel>Fruits</SelectLabel>
-                    <SelectItem value="Для собеседования">
-                      Для собеседования
-                    </SelectItem>
-                    <SelectItem value="Скрининг">
-                      Скрининг
-                    </SelectItem>
-                    <SelectLabel>Айтишка</SelectLabel>
-                    <SelectItem value="Развитие">
-                      Развитие
-                    </SelectItem>
-                    <SelectItem value="Важно">
-                      Важно
-                    </SelectItem>
-                  </SelectGroup>
-                </SelectContent>
-              </Select>
+              <SharedSelect label="Теги" placeholder="Укажите тэг" :options="TAGS"/>
             </div>
             <div class="w-full">
               <Label>Направление</Label>
-              <Select>
-                <SelectTrigger>
-                  <SelectValue placeholder="Select a fruit" />
-                </SelectTrigger>
-                <SelectContent>
-                  <SelectGroup>
-                    <SelectLabel>Fruits</SelectLabel>
-                    <SelectItem v-for="tag in TAGS_OBJECT" :value="tag.icon">
-                      <Icon size="20" class="align-middle" :name="tag.icon" color="white"/>
-                      {{ tag.alias }}
-                    </SelectItem>
-                  </SelectGroup>
-                </SelectContent>
-              </Select>
+              <SharedSelect label="Направление" placeholder="Укажите направление" :options="TAGS_OBJECT" />
             </div>
           </div>
-
-          <WidgetsTipTapEditor/>
         </DialogDescription>
       </DialogHeader>
 
